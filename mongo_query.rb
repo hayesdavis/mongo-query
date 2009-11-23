26866853579d382221a1362ef7b6fe212575979b
Hayes Davis <hayes@appozite.com>
2009-11-23 01:05:16 -0600

    First commit. Compiles SQL style queries into Mongo query documents. May not be complete.

---
+++ mongo_query.rb
@@ -0,0 +1,16 @@
+require 'treetop'
+require 'mongo_query_grammar'
+
+class MongoQuery
+  
+  class << self
+    
+    def compile(where_clause)
+      MongoQueryGrammarParser.new.parse(where_clause).to_mongo
+    rescue
+      nil
+    end
+    
+  end
+  
+end
